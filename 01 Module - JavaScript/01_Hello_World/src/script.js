33229b9260e4c630a3184d35e38cc81a822cf840
islom42 <islomnumanov.uz@gmail.com>
2024-02-21 10:02:33 +0300

    feat: Add basic js code

---
+++ 01 Module - JavaScript/01_Hello_World/src/script.js
@@ -0,0 +1,11 @@
+console.log('Hello wolrd');
+console.log("Hello wolrd");
+
+console.log(`Hey this is string literal`)
+
+// alert("hey");
+// const notification = alert()
+// console.log(typeof notification);
+
+const promtWindow = prompt("Say hello", "")
+console.log(typeof promtWindow);
