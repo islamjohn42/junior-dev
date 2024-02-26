39bf56b83dc8c5f92b2545d225f3f13d42cf466c
islom42 <islomnumanov.uz@gmail.com>
2024-02-27 01:40:27 +0300

    feat: Add second task

---
+++ 01 Module - JavaScript/02_Syntax/02_task/js/index.js
@@ -0,0 +1,18 @@
+// JavaScript-код с созданием переменных
+
+/* 
+Автор:
+Нуманов Исломбек (John)
+*/
+
+const myName = "Исломбек";
+const mySurname = "Нуманов";
+let myFavouriteDrink = "Зелёный чай с лимоном";
+let myFavouriteAnimal = "Кот";
+let myFavoriteProgrammingLanguage = "JavaScript"
+
+console.log(myName);
+console.log(mySurname);
+console.log(myFavouriteDrink);
+console.log(myFavouriteAnimal);
+console.log(myFavoriteProgrammingLanguage);
